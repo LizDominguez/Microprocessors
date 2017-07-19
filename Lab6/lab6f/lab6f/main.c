40742aa863ac4cf75d669cbdee187af5fd23852e
LizDominguez <liz.dominguez.leon@gmail.com>
2017-07-19 16:47:11 -0400

    accel and gyro work

--- Lab6/lab6f/lab6f/main.c
+++ Lab6/lab6f/lab6f/main.c
@@ -10,13 +10,12 @@
 #include <avr/interrupt.h>
 #include "LSM330.h"
 
-#define BSel  8		//1Mbps
-#define BScale  -3
+#define BSel  1		//1Mbps
+#define BScale  0
 
 
 volatile uint8_t accelDataReady = 0x00;
 volatile uint8_t gyroDataReady = 0x00;
-
 
 
 void CLK_init(void) {
@@ -28,23 +27,22 @@ void CLK_init(void) {
 	
 }
 
+
 void SPI_init()
 {
 	PORTA.DIR = 0xF8;		//Protocol Sel, others
 	PORTA.OUT = 0x00;		//Protocol sel SPI
 	PORTF.DIR = 0xBF;		//SSA, MOSI, SCK, MISO as input
 	PORTF.OUT = 0x18;		//SSA & SSG
-	
+	PORTC.DIR = 0x00;		//data direction
 	
 	SPIF.CTRL = SPI_ENABLE_bm |				//enable, data order msb
 				SPI_MASTER_bm |				//Master select
-				SPI_MODE0_bm  |				//mode 0 rising leading edge
-				SPI_PRESCALER_DIV4_gc;		//prescaler of 4
+				SPI_MODE_0_gc;				//mode 3 rising leading edge		
 				
 }
 
 void USART_init(void) {
-	
 	
 	PORTD.DIRSET = 0xF8;	//set TxD and RBG LED as output
 	PORTD.OUTSET = 0xF8;	//set TxD to high, LED to off
@@ -69,7 +67,6 @@ uint8_t spiWrite(uint8_t data) {
 			
 }
 
-
 uint8_t spiRead() {
 	
 	return spiWrite(0xFF);
@@ -80,11 +77,12 @@ uint8_t accRead(uint8_t reg){
 	
 	volatile uint8_t read;
 	
-	PORTF.OUTSET = 0x04;		//Sensor sel acc
 	PORTF.OUTCLR = 0x08;		//SSA clear
+	PORTF.OUTSET = 0x04;		//Sensor sel acc
 	
 	//RW, MS, AD[5:0], D[7:0]
-	read = spiWrite((reg | 0x80) & 0xF3);
+	spiWrite(lsm330_spiRead_strobe | (reg & 0x3F));
+	read = spiRead();
 	
 	PORTF.OUTSET = 0x08;		//SSA end transmission
 	
@@ -95,14 +93,12 @@ uint8_t accRead(uint8_t reg){
 
 void accWrite(uint8_t reg, uint8_t data) {
 	
-	volatile uint8_t write;
-	
-	PORTF.OUTSET = 0x04;		//Sensor sel acc
 	PORTF.OUTCLR = 0x08;		//SSA clear
+	PORTF.OUTSET = 0x04;		//Sensor sel acc
 	
 	//RW, MS, AD[5:0], D[7:0]
-	write = spiWrite(reg);
-	write = spiWrite(data);
+	spiWrite(lsm330_spiWrite_strobe | (reg & 0x3F));
+	spiWrite(data);
 	
 	PORTF.OUTSET = 0x08;		//SSA end transmission
 	
@@ -113,10 +109,13 @@ uint8_t gyroRead(uint8_t reg) {
 	
 	volatile uint8_t read;
 	
-	PORTF.OUTCLR = 0x10;		//SSG, sensor select gyro
+	PORTF.OUTCLR = 0x10;		//SSG
+	PORTF.OUTCLR = 0x04;		//Sensor sel gyro
+	
 	
 	//RW, MS, AD[5:0], D[7:0]
-	read = spiWrite((reg | 0x80) & 0xF3);
+	spiWrite(lsm330_spiRead_strobe | (reg & 0x3F));
+	read = spiRead();
 	
 	PORTF.OUTSET = 0x10;		//SSG end transmission
 	
@@ -126,13 +125,12 @@ uint8_t gyroRead(uint8_t reg) {
 
 void gyroWrite(uint8_t reg, uint8_t data) {
 	
-	volatile uint8_t read, write;
-	
-	PORTF.OUTCLR = 0x10;		//SSG, sensor select gyro
+	PORTF.OUTCLR = 0x10;		//SSG
+	PORTF.OUTCLR = 0x04;		//Sensor sel gyro
 	
 	//RW, MS, AD[5:0], D[7:0]
-	write = spiWrite(reg);
-	write = spiWrite(data);
+	spiWrite(lsm330_spiWrite_strobe | (reg & 0x3F));
+	spiWrite(data);
 	
 	PORTF.OUTSET = 0x10;		//SSG end transmission
 
@@ -146,26 +144,22 @@ void accel_init() {
 	Use CTRL_REG5_A to configure highest output rate and enable XYZ
 	*/
 	
-	PORTC.DIR = 0x00;		//data direction
 	PORTC.INTCTRL = 0x01;		//interrupt 0 low level
 	PORTC.INT0MASK = 0x80;		//Pin 7 interrupt
 	PORTC.PIN7CTRL = 0x02;		//sense falling edge
 	
 	
-	accWrite((~0x80 & CTRL_REG4_A & 0x3F), 0x01); //enable accelerometer
-	
-	accWrite((~0x80 & CTRL_REG4_A & 0x3F), (CTRL_REG4_A_DR_EN |		//route DRDY to INT1_A and enable INT1 rising edge
-											CTRL_REG4_A_IEA |		
-											CTRL_REG4_A_INT1_EN |
-											CTRL_REG4_A_STRT));			
+	accWrite(CTRL_REG4_A, (CTRL_REG4_A_DR_EN |		//route DRDY to INT1_A and enable INT1 rising edge
+							CTRL_REG4_A_IEA |		
+							CTRL_REG4_A_INT1_EN));	
+													
 			
-	accWrite((~0x80 & CTRL_REG5_A & 0x3F), (CTRL_REG5_A_ODR3 |		//configure 1600Hz output rate and enable XYZ
-											CTRL_REG5_A_ODR0 |
-											CTRL_REG5_A_ZEN |
-											CTRL_REG5_A_YEN |
-											CTRL_REG5_A_XEN));		
+	accWrite(CTRL_REG5_A, (CTRL_REG5_A_ODR3 |		//configure 1600Hz output rate and enable XYZ
+							CTRL_REG5_A_ODR0 |
+							CTRL_REG5_A_ZEN |
+							CTRL_REG5_A_YEN |
+							CTRL_REG5_A_XEN));		
 
-	
 }
 
 void gyro_init() {
@@ -181,23 +175,22 @@ void gyro_init() {
 	PORTA.OUT = 0x08;				//gyro enable
 	PORTA.INTCTRL = 0x01;			//interrupt 0 low level
 	PORTA.INT0MASK = 0x02;			//Pin 1 interrupt
-	PORTA.DIRSET &= ~0x02;			//data direction
 	PORTA.PIN7CTRL = 0x01;			//sense rising edge
 
 	
-	gyroWrite((~0x80 & CTRL_REG1_G & 0x3F), 0x01 );
+	gyroWrite(CTRL_REG1_G, (CTRL_REG1_G_DR1 |		//highest 100Hz, enable XYZ, normal mode
+							CTRL_REG1_G_DR0 |
+							CTRL_REG1_G_PD	|
+							CTRL_REG1_G_ZEN |
+							CTRL_REG1_G_YEN |
+							CTRL_REG1_G_XEN));	
+													
 	
-	gyroWrite((~0x80 & CTRL_REG1_G & 0x3F), (CTRL_REG1_G_DR1 |		//highest 100Hz, enable XYZ, normal mode
-											CTRL_REG1_G_DR0 |
-											CTRL_REG1_G_PD	|
-											CTRL_REG1_G_ZEN |
-											CTRL_REG1_G_YEN |
-											CTRL_REG1_G_XEN));			
+	gyroWrite(CTRL_REG3_G, CTRL_REG3_G_I2_DRDY);	//set I2_DRDY
 	
-	gyroWrite((~0x80 & CTRL_REG3_G & 0x3F), CTRL_REG3_G_I2_DRDY);	//set I2_DRDY
 	
-	gyroWrite((~0x80 & CTRL_REG4_G & 0x3F), (CTRL_REG4_G_FS1 |
-											CTRL_REG4_G_FS0));		//2000 dps
+	gyroWrite(CTRL_REG4_G, (CTRL_REG4_G_FS1 |
+							CTRL_REG4_G_FS0));		//2000 dps
 	
 }
 
@@ -221,28 +214,39 @@ ISR(PORTA_INT0_vect) {
 	PORTA.INTFLAGS |= 0x01;
 }
 
-void delay_50us(void)
-{
-	volatile uint32_t ticks;				//Volatile prevents compiler optimization
-	for(ticks = 0; ticks <= 2000; ticks++);	//increment 2e6 times -> ~ 1 sec
-}
 
 int main(void)
 {
+	PMIC.CTRL = 0x01;			//low level interrupts	
 	CLK_init();
-    SPI_init();
 	USART_init();
-	accel_init();
+    SPI_init();
+	
+	gyroWrite(CTRL_REG1_G, 0x00);	//reset gyro
+	accWrite(CTRL_REG4_A, 0x01);	//reset accelerometer
+	
 	gyro_init();
-	PMIC.CTRL = 0x01;			//low level interrupts	 
+	accel_init();
+	 
 	sei();
 	
 	volatile uint8_t ACCEL_X_H, ACCEL_X_L, ACCEL_Y_H, ACCEL_Y_L, ACCEL_Z_H, ACCEL_Z_L;
 	volatile uint8_t GYRO_X_H, GYRO_X_L, GYRO_Y_H, GYRO_Y_L, GYRO_Z_H, GYRO_Z_L;
 	
-    while(1){
+    while(1){	
 		
-		if(accelDataReady == 0x01){
+		if(gyroDataReady == 0x01){
+			GYRO_X_H = gyroRead(OUT_X_H_G);
+			GYRO_X_L = gyroRead(OUT_X_L_G);
+			GYRO_Y_H = gyroRead(OUT_Y_H_G);
+			GYRO_Y_L = gyroRead(OUT_Y_L_G);
+			GYRO_Z_H = gyroRead(OUT_Z_H_G);
+			GYRO_Z_L = gyroRead(OUT_Z_L_G);
+			
+			gyroDataReady = 0x00;
+		}
+		
+		else if(accelDataReady == 0x01){
 			ACCEL_X_H = accRead(OUT_X_H_A);
 			ACCEL_X_L = accRead(OUT_X_L_A);
 			ACCEL_Y_H = accRead(OUT_Y_H_A);
@@ -254,16 +258,6 @@ int main(void)
 			
 		}
 		
-		else if(gyroDataReady == 0x01){
-			GYRO_X_H = gyroRead(OUT_X_H_G);
-			GYRO_X_L = gyroRead(OUT_X_L_G);
-			GYRO_Y_H = gyroRead(OUT_Y_H_G);
-			GYRO_Y_L = gyroRead(OUT_Y_L_G);
-			GYRO_Z_H = gyroRead(OUT_Z_H_G);
-			GYRO_Z_L = gyroRead(OUT_Z_L_G);
-			
-			gyroDataReady = 0x00;
-		}
 		
 		send_char(0x03);
 		send_char(ACCEL_X_L);
